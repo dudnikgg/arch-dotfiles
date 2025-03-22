d5f6b70ccd170d981762f0bcd44eb8ebfa103f93
Denys Dudnik <dudnik.gg@gmail.com>
2025-03-22 15:39:29 +0200

    add some aliases, turn off blur hypr tmp

--- zshrc/zsh-configs/aliases.sh
+++ zshrc/zsh-configs/aliases.sh
@@ -11,13 +11,19 @@ alias gm="git merge"
 alias ga="git add"
 alias gph="git push origin HEAD"
 
+# Git diff
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gdst="git diff --stat --staged"
+
 alias _git_current_branch="git rev-parse --abbrev-ref HEAD"
 alias gpl='git pull origin $(_git_current_branch)'
 
 alias ls='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first'
 alias ll='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first -l --git -h'
+alias lls='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first --total-size -l --git -h'
 alias la='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first -a'
-alias lla='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first -a -l --git -h'
+alias lla='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first --total-size -a -l --git -h'
 
 # Function to create a new branch with a specified format
 function gcb() {
@@ -32,3 +38,20 @@ function gcb() {
 
 # Alias to call the function
 alias gcb="gcb"
+
+
+alias rcp="rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1"
+alias rmv="rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 --remove-source-files"
+
+# Rsync Copy (cpr) - Works with Remote Paths
+function cp-remote() {
+  local remote="$1"
+  local host="$2"
+
+  rsync --archive -hh --partial --stats --progress --modify-window=1 --exclude=node_modeles "${remote}" "${host}"
+}
+
+# Rsync Move (mvr) - Works with Remote 
+# mvr() {
+#  rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 --remove-source-files "$1"
+# }
