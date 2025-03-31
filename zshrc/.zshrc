67f818aa50c304670689cdfd85922090ceddc0ac
Denys Dudnik <dudnik.gg@gmail.com>
2025-03-31 20:26:44 +0300

    updates, waybar new modules

--- zshrc/.zshrc
+++ zshrc/.zshrc
@@ -20,3 +20,4 @@ done
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+export PATH="$HOME/bin:$PATH"
--- zshrc/zsh-configs/init.sh
+++ zshrc/zsh-configs/init.sh
@@ -22,8 +22,11 @@ export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 export EDITOR=nvim
 export VISUAL=${EDITOR}
 
+# Add nvm
 source /usr/share/nvm/init-nvm.sh
 
 # To customize promt, run 'p10k configure' or edit ~/.p10k.zsh
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
+path+=('/home/dudnikgg/.local/bin')
+export PATH
